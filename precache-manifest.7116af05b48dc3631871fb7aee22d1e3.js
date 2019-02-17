e4779a7051f465e3bc04136e21031eefdbbffc4b
Daniel Kawalsky <dkawalsk@usc.edu>
2019-02-16 23:19:34 -0800

    Updates

---
+++ precache-manifest.7116af05b48dc3631871fb7aee22d1e3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "35b5d012db93dc47fea924feecfff603",
+    "url": "/website/static/media/TopCenturyDiasporaBreakdown.35b5d012.png"
+  },
+  {
+    "revision": "5f86a16dd42fea854b32bdebe47d114f",
+    "url": "/website/static/media/Ninth_Academy_Awards.5f86a16d.jpg"
+  },
+  {
+    "revision": "1130ddd8c0ff45d88cf94dc97bc6a967",
+    "url": "/website/static/media/Doughnut.1130ddd8.png"
+  },
+  {
+    "revision": "cc6af7288156b3de604548bd14f7c396",
+    "url": "/website/static/media/Credits_Blur_Graph_Superimposed.cc6af728.png"
+  },
+  {
+    "revision": "ac147926a7a992beeb884969d1a7bf83",
+    "url": "/website/static/media/Credits Blur.ac147926.png"
+  },
+  {
+    "revision": "738c5e760cdb5d1c1801",
+    "url": "/website/static/js/runtime~main.738c5e76.js"
+  },
+  {
+    "revision": "32a040632da161a0e395",
+    "url": "/website/static/js/main.32a04063.chunk.js"
+  },
+  {
+    "revision": "549dbbe7d301c18482e9",
+    "url": "/website/static/js/2.549dbbe7.chunk.js"
+  },
+  {
+    "revision": "32a040632da161a0e395",
+    "url": "/website/static/css/main.4a75c59d.chunk.css"
+  },
+  {
+    "revision": "1e4e523bca55d8b34d44646580d3200a",
+    "url": "/website/index.html"
+  }
+];
