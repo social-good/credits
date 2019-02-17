84c2ba1bf9d92ff8ef998b15504577db64336d2d
Daniel Kawalsky <dkawalsk@usc.edu>
2019-02-16 22:17:10 -0800

    added gh pages stuff to git

--- src/components/closeups/Closeup0.js.jsx
+++ src/components/closeups/Closeup0.js.jsx
@@ -14,6 +14,7 @@ export default class Closeup0 extends Component {
 				<div>
 					<p> Quote from previous site. </p>
 					<p> What are we looking at and why? ... which also happens to pair really well with the above image ... consider changing the title.</p>
+					<p> I see white people gif. You probably see a lot of white people. (link) </p>
 					<img src={require('../../media/images/Ninth_Academy_Awards.jpg')} alt={'Cannot display this.'}/>
 					<p> Credits seem to scroll by... and nobody cares </p>
 					<img src={require('../../media/images/Credits Blur.png')} alt={'Cannot display this.'}/>
