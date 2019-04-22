3ec7e33afd063681cb94e6e4106fece6588245a9
Daniel Kawalsky <dkawalsk@usc.edu>
2019-04-22 16:41:44 -0700

    removed github link'

--- src/components/homepage/TopBar.js.jsx.js
+++ src/components/homepage/TopBar.js.jsx.js
@@ -11,7 +11,6 @@ class TopBar extends Component {
 					<Link to="/"><div className='topbar-tab-box'>HOME</div></Link>
 					<Link to="/about"><div className='topbar-tab-box'>ABOUT</div></Link>
 					<div className='topbar-tab-box'>SEARCH</div>
-					<a href="https://github.com/social-good"><div className='topbar-tab-box'>GITHUB</div></a>
 				</div>
 				<div className="topbar-bottom-bar"></div>
 			</div>
